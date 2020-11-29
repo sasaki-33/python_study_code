7cddeef54f0ea4486a8b86931865d8960378bb8e
sasaki-33 <iron12133@yahoo.co.jp>
2020-11-29 19:32:06 +0900

    selenium使用

---
+++ YouTube_login.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import pyautogui
+driver = webdriver.Chrome('webdriverのファイル')
+
+#YouTubeへ
+driver.get('https://www.youtube.com/')
+pyautogui.sleep(5)
+#ログインボタンのXpathを取得、クリック
+i1 = driver.find_element_by_xpath('//*[@id="buttons"]/ytd-button-renderer/a')
+i1.click()
+pyautogui.sleep(3)
+#メールアドレス入力の要素を取得、書き込み
+i2 = driver.find_element_by_name('identifier')
+i2.send_keys('メールアドレス')
+#次へのボタンのXpathを取得
+i3 = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button')
+i3.click()
+#パスワードの要素を取得、以下同様
