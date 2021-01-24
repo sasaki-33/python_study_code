daa19f8c119c84e2c81c30d8b48a10362df6cac9
sasaki-33 <iron12133@yahoo.co.jp>
2021-01-24 23:05:08 +0900

    Str_math途中

--- str_math.py
+++ str_math.py
@@ -4,38 +4,43 @@ example = '24+45'
 expression = [i for i in example]
 
 #数字が複数桁の時、結合し一つの文字として扱う
-basic = 0
-#リストの最後を示す文字を追加
-expression.append('%')
-moji = 0
-#スタックの機能を果たすリストを用意
-stack = []
-while moji < len(expression):
-    #文字が数字、もしくは最後の要素か
-    if '0' <= expression[moji] <= '9' or expression[moji] == '%':
-        stack.append(expression[moji])
-    #文字が演算子か    
-    if expression[moji] == '*' or expression[moji] == '/' or expression[moji] == '+' or expression[moji] == '-' or expression[moji] == '(' or expression[moji] == ')':
-        stack.append(expression[moji])
 
-    #スタックの最後の要素が、演算子またはexpressionの最後の要素ならば、それ以前の文字を結合
-    if stack[-1] == '*' or stack[-1] == '/' or stack[-1] == '+' or stack[-1] == '-' or stack[-1] == '(' or stack[-1] == ')' or stack[-1] == '%' :
-        t = 0
-        while t < len(stack)-2:
-            total = stack[t] + stack[t+1]
-            t += 1
-        expression[basic] = total
-        #結合することで余分となったexpressionの要素を削除
-        while moji > basic + 1:
-            expression.remove(expression[moji-1])
-            moji -= 1
-        #演算子以降の文字の結合のためにスタックを空に戻す        
-        stack.clear()
-        basic = moji + 1
-    moji += 1
+def henkan(ex):
+    basic = 0
+    #リストの最後を示す文字を追加
+    ex.append('%')
+    moji = 0
+    #スタックの機能を果たすリストを用意
+    stack = []
+    while moji < len(ex):
+        #文字が数字、もしくは最後の要素か
+        if '0' <= ex[moji] <= '9' or ex[moji] == '%':
+            stack.append(ex[moji])
+        #文字が演算子か    
+        if ex[moji] == '*' or ex[moji] == '/' or ex[moji] == '+' or ex[moji] == '-' or ex[moji] == '(' or ex[moji] == ')':
+            stack.append(ex[moji])
 
-expression.remove(expression[-1])
-print(expression)
+        #スタックの最後の要素が、演算子またはexpressionの最後の要素ならば、それ以前の文字を結合
+        if stack[-1] == '*' or stack[-1] == '/' or stack[-1] == '+' or stack[-1] == '-' or stack[-1] == '(' or stack[-1] == ')' or stack[-1] == '%' :
+            t = 0
+            while t < len(stack)-2:
+                total = stack[t] + stack[t+1]
+                t += 1
+            ex[basic] = total
+            #結合することで余分となったexpressionの要素を削除
+            while moji > basic + 1:
+                ex.remove(ex[moji-1])
+                moji -= 1
+            #演算子以降の文字の結合のためにスタックを空に戻す        
+            stack.clear()
+            basic = moji + 1
+        moji += 1
+    #exの最後を示す要素を削除
+    expression.remove(ex[-1])
+    return ex
+
+i = henkan(expression)
+print(i)
 """
 #int型の数値を保持するリストを用意
 value = []
