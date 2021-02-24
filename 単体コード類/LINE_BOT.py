4a2c3faf5471def296f987772d68ca69ebf05b85
sasaki-33 <iron12133@yahoo.co.jp>
2021-02-24 23:24:25 +0900

    Line_Bot追加

---
+++ 単体コード類/LINE_BOT.py
@@ -0,0 +1,18 @@
+import json
+file = open('info.json','r')
+info = json.load(file)
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID = info['USER_ID']
+    jyouhou = ''
+    messages = TextSendMessage(text=jyouhou)
+    line_bot_api.push_message(USER_ID,messages = messages)
+
+if __name__ == "__main__":
+    main()
